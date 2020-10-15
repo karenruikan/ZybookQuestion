1615ff6cb6cfa18977cba049aa5db9f59d009617
Sarah Shamsie <sshamsie@Sarahs-MBP.fios-router.home>
2020-10-15 13:48:23 -0400

    this is the second commit to the remote repo. check the GitScript.java file for code

--- GitQuizTester/src/GitScript.java
+++ GitQuizTester/src/GitScript.java
@@ -5,6 +5,7 @@ public class GitScript {
 	public static void main(String[] args) {
 		
 		System.out.println("This is the first commit");
+		System.out.println("This is the second commit");
 		
 	}
 
